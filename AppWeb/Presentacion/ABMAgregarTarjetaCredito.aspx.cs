9b6440c7169cdcdd3224dfa66262bc26ab48de65
Edward Rodriguez <edwarderf@gmail.com>
2019-08-23 12:04:50 -0300

    ABMAgregarTarjetaCredito - ActivoBotones y LimpioFormulario V1

--- AppWeb/Presentacion/ABMAgregarTarjetaCredito.aspx.cs
+++ AppWeb/Presentacion/ABMAgregarTarjetaCredito.aspx.cs
@@ -9,6 +9,43 @@ public partial class ABMAgregarTarjetaCredito : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+            this.LimpioFormulario();
+    }
 
+    private void ActivoBotonesM()
+    {
+        btnModificar.Enabled = true;
+
+        btnAlta.Enabled = false;
+        btnBuscar.Enabled = false;
+
+        txtCI.Enabled = false;
+    }
+
+    private void ActivoBotonesA()
+    {
+        btnBuscar.Enabled = false;
+        btnAlta.Enabled = true;
+        btnModificar.Enabled = false;
+
+        txtCI.Enabled = false;
+    }
+
+    private void LimpioFormulario()
+    {
+        btnBuscar.Enabled = true;
+        txtCI.Enabled = true;
+
+        btnAlta.Enabled = false;
+        btnModificar.Enabled = false;
+
+        txtCI.Text = "0";
+        txtFechaVencimiento.Text = "";
+        txtPersonalizada.Text = "";
+        txtCategoria.Text = "";
+        txtCreditoDisponible.Text = "";
+
+        lblError.Text = "";
     }
 }
